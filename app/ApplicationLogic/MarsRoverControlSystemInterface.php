31e7d9ce92eb7b80a65f7b0943dc071a4b0fe619
Giorgio <70840073+giobongio@users.noreply.github.com>
2024-04-22 17:26:19 +0200

    feat: added wrap to control system interface

--- app/ApplicationLogic/MarsRoverControlSystemInterface.php
+++ app/ApplicationLogic/MarsRoverControlSystemInterface.php
@@ -7,6 +7,7 @@ use App\Data\Position;
 interface MarsRoverControlSystemInterface
 {
     public function sendCommands(array $commands): array;
+    public function wrap(): array;
     public function setPosition(Position $position): bool;
     public function getPosition(): Position;
 }
